007cf2f6e8f523c62da7916fa679c009924332ef
andreicamargop <andreicamargopinto2@hotmail.com>
2025-08-21 21:50:59 -0300

    a

--- aula-desenvolvimento-sistemas-senai/multi-page/src/main.jsx
+++ aula-desenvolvimento-sistemas-senai/multi-page/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+
+//    REACT ROUTER
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: <Home></Home>,
+  },
+  {
+    path: "sobre",
+    Component:<Sobre></Sobre>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />,
+    {/* <App /> */}
   </StrictMode>,
 )
